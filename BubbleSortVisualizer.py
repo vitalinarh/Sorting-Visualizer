2e1d4e6cc07a7f3e528f97443eea43792b373220
vitalinarh <uc2017255810@student.uc.pt>
2020-07-28 23:07:50 +0100

    Update 1

--- BubbleSortVisualizer.py
+++ BubbleSortVisualizer.py
@@ -13,6 +13,15 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 200)
 
+bubble_sort = True          # 0
+selection_sort = False      # 1
+insertion_sort = False      # 2
+merge_sort = False          # 3
+quick_sort = False          # 4
+heap_sort = False           # 5
+
+buttons = []
+
 def setup(n):
 
     values = [0 for i in range(n)]
@@ -30,23 +39,12 @@ def swap(values, i, j):
 
     return values
 
-if __name__=="__main__":
-
-    global SCREEN, CLOCK
-    pygame.init()
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-    pygame.display.set_caption('Bubble Sort Visualizer')
-    CLOCK = pygame.time.Clock()
-    SCREEN.fill(BLACK)
-
-    n = 500
-
-    values = setup(n)
+# BUBBLE SORT
+def bubbleSort(values):
 
     for i in range(n):
 
         for j in range(n - 1):
-            CLOCK.tick(1000)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -60,10 +58,90 @@ if __name__=="__main__":
 
                 SCREEN.fill(BLACK)
 
-                for i in range(len(values)):
+                for i in range(n):
                     start = (i * (WINDOW_WIDTH / n) + (WINDOW_WIDTH / n), WINDOW_HEIGHT)
                     end = (i * (WINDOW_WIDTH / n) + (WINDOW_WIDTH / n), WINDOW_HEIGHT - values[i])
-                    pygame.draw.line(SCREEN, WHITE, start, end, 1)
+                    pygame.draw.line(SCREEN, WHITE, start, end, 2)
 
 
-            pygame.display.update()
+
+                CLOCK.tick(10000)
+                pygame.display.update()
+
+    return
+
+#SELECTION SORT
+
+def drawText(message, x, y, color):
+
+    font = pygame.font.Font('freesansbold.ttf', 14)
+    text = font.render(message, True, color, BLACK)
+
+    textRect = text.get_rect()
+    textRect.center = (x, y)
+
+    SCREEN.blit(text, textRect)
+
+def display_buttons():
+    # button 0
+    rect = pygame.Rect(1000, 40, 100, 40)
+    pygame.draw.rect(SCREEN, WHITE, rect, 2)
+    if bubble_sort:
+        drawText('Bubble Sort', 1050, 60, RED)
+    else:
+        drawText('Bubble Sort', 1050, 60, WHITE)
+
+def check_click_btn(pos):
+
+    global bubble_sort
+    x = pos[0]
+    y = pos[1]
+
+    # x = 1000 y = 40
+    if x >= 1000 and x <= 1100 and y >= 40 and y <= 80:
+        print("clicked")
+        bubble_sort = not bubble_sort
+
+if __name__=="__main__":
+
+    global SCREEN, CLOCK
+    pygame.init()
+    SCREEN = pygame.display.set_mode((WINDOW_WIDTH + 300, WINDOW_HEIGHT))
+    pygame.display.set_caption('Bubble Sort Visualizer')
+    CLOCK = pygame.time.Clock()
+    SCREEN.fill(BLACK)
+
+    n = 100
+
+    values = setup(n)
+
+    for i in range(n):
+        start = (i * (WINDOW_WIDTH / n) + (WINDOW_WIDTH / n), WINDOW_HEIGHT)
+        end = (i * (WINDOW_WIDTH / n) + (WINDOW_WIDTH / n), WINDOW_HEIGHT - values[i])
+        pygame.draw.line(SCREEN, WHITE, start, end, 2)
+
+    # bubbleSort(values)
+
+    '''
+    for i in range(n):
+        start = (i * (WINDOW_WIDTH / n) + (WINDOW_WIDTH / n), WINDOW_HEIGHT)
+        end = (i * (WINDOW_WIDTH / n) + (WINDOW_WIDTH / n), WINDOW_HEIGHT - values[i])
+        pygame.draw.line(SCREEN, GREEN, start, end, 2)
+        CLOCK.tick(50)
+        pygame.display.update()'''
+
+    while True:
+
+        display_buttons()
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                check_click_btn(pos)
+
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.update()
